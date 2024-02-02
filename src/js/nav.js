e4b696bad1cd09b1b48327f2ed5c45b3937189a2
shcampus02 <s52581@edu.campus02.at>
2024-02-02 12:42:29 +0100

    Finished Responsive Styling for Navigation close #12

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,20 +1,9 @@
 window.onload = function() {
-    var hamburger = document.getElementById('hamburger-icon');
-    var closeMenu = document.getElementById('close-icon');
-    var nav = document.getElementById('navigation').getElementsByTagName('ul')[0];
-    var logo = document.getElementById('logo_header');
+    const menu_btn = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile-nav')
 
-    hamburger.addEventListener('click', function() {
-        nav.classList.add('open');
-        hamburger.classList.add('open'); /* Hide hamburger */
-        closeMenu.classList.add('open'); /* Show close-menu */
-        logo.classList.add('hidden'); /* Hide logo when nav is open */
-    });
-
-    closeMenu.addEventListener('click', function() {
-        nav.classList.remove('open');
-        hamburger.classList.remove('open'); /* Show hamburger */
-        closeMenu.classList.remove('open'); /* Hide close-menu */
-        logo.classList.remove('hidden'); /* Show logo when nav is closed */
-    });
-};
+    menu_btn.addEventListener('click', function () {
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    })
+}
