2d4ffee6d96f376e7ff3726012672d0c4d3f6cb4
shcampus02 <s52581@edu.campus02.at>
2024-02-03 13:54:11 +0100

    Automatically collapse drop-down navbar menu on mobile when back button pressed
    close #37

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,13 +1,27 @@
 window.onload = function() {
     const menu_btn = document.querySelector('.hamburger');
-    const mobile_menu = document.querySelector('.mobile-nav')
+    const mobile_menu = document.querySelector('.mobile-nav');
 
     menu_btn.addEventListener('click', function () {
+        toggleMenu();
+    })
+
+    window.addEventListener('pageshow', function() {
+        if (menu_btn.classList.contains('is-active')) {
+            mobile_menu.classList.add('notransition');
+            toggleMenu();
+
+            void mobile_menu.offsetWidth;
+
+            mobile_menu.classList.remove('notransition');
+        }
+    });
+
+    function toggleMenu() {
         menu_btn.classList.toggle('is-active');
         mobile_menu.classList.toggle('is-active');
-
-        // Aktualisiere aria-expanded
+        // Messing with aria-expanded attribute
         const ariaExpanded = menu_btn.getAttribute('aria-expanded') === 'true';
         menu_btn.setAttribute('aria-expanded', !ariaExpanded);
-    })
+    }
 }
